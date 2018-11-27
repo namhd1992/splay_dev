98131171bbb73973d64c6441612391321319d77c
stpnghd <buivannam1992@gmail.com>
2018-11-27 15:55:28 +0700

    fix

--- src/components/page/Coin.jsx
+++ src/components/page/Coin.jsx
@@ -62,17 +62,12 @@ class CoinComponent extends React.Component {
 		var coin=localStorage.getItem("Coin");
         var pakageCoin=+event.target.value;
         var ratioExchange=this.props.data.ratioExchange;
-        var packageExchangeXUs=this.props.data.packageExchangeXUs;
-        if(packageExchangeXUs.indexOf(pakageCoin)===-1){
-            alert("Gói bạn chọn không tồn tại");
+        if(+coin===1){
+            this.setState({pakage:pakageCoin, value:pakageCoin, price:(pakageCoin/ratioExchange), type:"scoinToGame",packageXO:pakageCoin, packageXU:(pakageCoin/ratioExchange)});
+        }else if(+coin===2){
+            this.setState({pakage:pakageCoin, value:pakageCoin, price:ratioExchange*pakageCoin, type:"gameToScoin",packageXU:pakageCoin, packageXO:pakageCoin*ratioExchange});
         }else{
-            if(+coin===1){
-                this.setState({pakage:pakageCoin, value:pakageCoin, price:(pakageCoin/ratioExchange), type:"scoinToGame",packageXO:pakageCoin, packageXU:(pakageCoin/ratioExchange)});
-            }else if(+coin===2){
-                this.setState({pakage:pakageCoin, value:pakageCoin, price:ratioExchange*pakageCoin, type:"gameToScoin",packageXU:pakageCoin, packageXO:pakageCoin*ratioExchange});
-            }else{
-                this.setState({pakage:pakageCoin, value:pakageCoin, price:ratioExchange*pakageCoin, type:"gameToScoin",packageXU:pakageCoin, packageXO:pakageCoin*ratioExchange});
-            }
+            this.setState({pakage:pakageCoin, value:pakageCoin, price:ratioExchange*pakageCoin, type:"gameToScoin",packageXU:pakageCoin, packageXO:pakageCoin*ratioExchange});
         }
        
     }
