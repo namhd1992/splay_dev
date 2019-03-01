98e3619ef4bff705032cf4a54d2ef74d5df84eb7
namhd1992 <stpnghd.vn@gmail.com>
2019-03-01 15:06:38 +0700

    fix

--- src/components/page/Event.jsx
+++ src/components/page/Event.jsx
@@ -95,7 +95,7 @@ class EventComponent extends React.Component {
 		}
 		window.location.replace(
 			// `https://graph.vtcmobile.vn/oauth/authorize?client_id=707fece431a0948c498d43e881acd2c5&redirect_uri=${window.location.protocol}//${window.location.host}&action=logout&agencyid=0`,
-			`http://sandbox.graph.vtcmobile.vn/oauth/authorize?client_id=4e7549789b14693eda4e019faaa0c446&redirect_uri=${window.location.protocol}//${window.location.host}/${currentPath}&action=logout&agencyid=0`
+			`http://sandbox.graph.vtcmobile.vn/oauth/authorize?client_id=4e7549789b14693eda4e019faaa0c446&redirect_uri=${window.location.protocol}//${window.location.host}${currentPath}&action=logout&agencyid=0`
 		);
 	}
 
